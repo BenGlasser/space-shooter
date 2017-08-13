8ce79624697b1b7ce665414b6ab0752ca4ffa185
Ben Glasser <ben@benglasser.com>
2017-08-13 17:32:24 -0600

    update all the things

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+
+    // Use this for initialization
+    void Start () {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        else
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.tag.Equals("boundary"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        if (other.tag.Equals("Bolt"))
+        {
+            gameController.AddScore(scoreValue);
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+        if (other.tag.Equals("Player"))
+        {
+            Instantiate(playerExplosion, transform.position, transform.rotation);
+            gameController.GameOver();
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public List<GameObject> hazard;
+    public Vector3 spawnValues;
+    public int hazardcount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    // Use this for initialization
+    void Start ()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+
+        GameObject scoreObject = GameObject.FindWithTag("score");
+        GameObject gameoverObject = GameObject.FindWithTag("gameover");
+        GameObject restartObject = GameObject.FindWithTag("restart");
+        if (scoreObject != null)
+        {
+            scoreText = scoreObject.GetComponent<GUIText>();
+            gameOverText = gameoverObject.GetComponent<GUIText>();
+            restartText = restartObject.GetComponent<GUIText>();
+        }
+        else
+        {
+            Debug.Log("Cannot find 'Score Text object' script");
+        }
+        score = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (restart)
+        {
+            if (Input.GetKeyDown (KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+	}
+
+    IEnumerator SpawnWaves()
+    {
+       yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardcount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
